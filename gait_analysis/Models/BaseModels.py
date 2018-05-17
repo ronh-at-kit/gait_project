1f94e7462b50fc1748c06f1972c28ce0bb7bda47
Sandro Braun <supermario94123@gmail.com>
2018-05-17 11:05:23 +0200

    * base model

---
+++ gait_analysis/Models/BaseModels.py
@@ -0,0 +1,17 @@
+class AbstractGaitModel():
+    '''
+    AbstractBaseModelClass
+    '''
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def validate(self):
+        pass
+
+
+    def predict(self):
+        pass
