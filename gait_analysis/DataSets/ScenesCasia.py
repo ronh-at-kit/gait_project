fcffc7a745c22c289360482533c07da5e714b99f
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-03-08 10:09:50 +0100

    recover state of scenes casia to prev commit 3cdebb4

--- gait_analysis/DataSets/ScenesCasia.py
+++ gait_analysis/DataSets/ScenesCasia.py
@@ -1,13 +1,10 @@
 import os
-from memory_profiler import profile
-
 import cv2
 from os import listdir
 from os.path import join
 from torch.utils.data import Dataset
 
 from gait_analysis.utils.files import format_data_path
-from gait_analysis.utils.data_loading import read_image, extract_pnum
 import gait_analysis.settings as settings
 from gait_analysis.Config import Config
 
@@ -17,7 +14,7 @@ class ScenesCasia(Dataset):
     '''
     TumGAID_Dataset loader
     '''
-    __slots__ = 'config' , 'images_dir', 'dataset_items', 'options'
+
     def __init__(self, dataset_items, transform=None):
 
         self.config = Config()
@@ -31,7 +28,7 @@ class ScenesCasia(Dataset):
 
     def __len__(self):
         return len(self.dataset_items)
-    # @profile()
+
     def __getitem__(self, idx):
         '''
         returns a a dictionary with the outputs that were configured int the constructor
@@ -45,13 +42,13 @@ class ScenesCasia(Dataset):
         output = {}
 
         # Loading scene images
-        images = self.load_scene(dataset_item)
+        images = self._load_scene(dataset_item)
         return images
 
     def set_option(self,key, value):
         self.options[key] = value
-    # @profile()
-    def load_scene(self , dataset_item):
+
+    def _load_scene(self, dataset_item):
         '''
         Loads scene sequence given a dataset item.
         :param dataset_item: the tuple indicating the dataset item
@@ -71,18 +68,28 @@ class ScenesCasia(Dataset):
             return join(scene_folder , '{}-{:03d}'.format(sequence , angle) , \
                     '{:03d}-{}-{:03d}_frame_{:03d}.jpg'.format(p_num , sequence , angle , i))
 
+        scene_files = []
         if 'valid_indices' in self.options:
             valid_indices = self.options['valid_indices']
-            scene_files = [compose_image_filename(angle , i) for i , valid in enumerate(valid_indices) if valid]
+            scene_files += [compose_image_filename(angle , i) for i , valid in enumerate(valid_indices) if valid]
         else:
-            scene_files = [join(scene_folder, '{}-{:03d}'.format(sequence , angle) , f) for f in listdir(scene_folder) if f.endswith('.jpg')]
+            scene_files += [join(scene_folder, '{}-{:03d}'.format(sequence , angle) , f) for f in listdir(scene_folder) if f.endswith('.jpg')]
             scene_files.sort()
-        scene_images = []
-        for f in scene_files:
-            image = read_image(f)
-            scene_images.append(image)
+
+        def read_image(im_file):
+            if not os.path.isfile(im_file):
+                raise ValueError('{} don\'t exist.'.format(im_file))
+            im = cv2.imread(im_file, -1)
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+            return im
+        scene_images = [read_image(f) for f in scene_files]
         return scene_images
 
+
+def extract_pnum(abspath):
+    path = os.path.basename(abspath)
+    p_num = path[-7:-4]
+    return int(p_num)
 
 
 if __name__ == '__main__':
